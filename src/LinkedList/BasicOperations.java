a8127d6d0820b1423cfe610740b2594614da9d40
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-11-18 12:31:46 +0530

    delete node from start

--- src/LinkedList/BasicOperations.java
+++ src/LinkedList/BasicOperations.java
@@ -3,50 +3,56 @@ import java.util.Scanner;
 
 public class BasicOperations {
 
-    //** Global variables **////
-    static Scanner sc = new Scanner(System.in); // a global scanner class to perform input operations
-    // Create a global node
-    static Node headNode = null; // Global node pointing to the head of linked list
+    private Node headNode = null;
+    private static final Scanner sc = new Scanner(System.in);
+    public BasicOperations(Node headNode){
+        this.headNode = headNode;
+    }
 
-    public static void insertNodeAtEnd(){
-        int data = sc.nextInt();
-        Node traverseNode = headNode;
-        if(headNode == null){
-            // only once when the linked list is not initialised
-             headNode = new Node(data,null);
-        }
-        else {
-            while (traverseNode.next!=null){
-                traverseNode = traverseNode.next;
+    public Node getHeadNode() {
+        return headNode;
+    }
+
+    public void insertNodeAtEnd() {
+        System.out.println("How many nodes do you want to enter?");
+        int n = sc.nextInt();
+        System.out.println("Enter nodes :");
+        for(int i=0;i<n;++i){
+            int data = sc.nextInt();
+            Node traverseNode = headNode;
+            if (headNode == null) {
+                // only once when the linked list is not initialised
+                headNode = new Node(data, null);
+            } else {
+                while (traverseNode.next != null) {
+                    traverseNode = traverseNode.next;
+                }
+                traverseNode.next = new Node(data, null);
             }
-            traverseNode.next = new Node(data,null);
         }
     }
 
-    public static void displayNodes(){
+    public void displayNodes() {
         Node traverseNode = headNode;
-        while(traverseNode!=null){
-            System.out.print(traverseNode.data+"->");
+        while (traverseNode != null) {
+            System.out.print(traverseNode.data + "->");
             traverseNode = traverseNode.next;
         }
         System.out.print("null");
+        System.out.println();
     }
 
-    public static void insertNodeAtBeginning(){
+    public void insertNodeAtBeginning() {
         System.out.println("Enter node at beginning");
         int data = sc.nextInt();
-        headNode = new Node(data,headNode);
+        headNode = new Node(data, headNode);
     }
 
-    public static void main(String[] args) {
-         //******  Insert 5 nodes at end of the node *****//
-        System.out.println("Enter 5 nodes");
-        for (int i = 0; i < 5; i++) {
-            insertNodeAtEnd();
+    public void deleteNodeFromStart() {
+        if (headNode == null || headNode.next == null) {
+            headNode = null;
+        } else {
+            headNode = headNode.next;
         }
-        // *** Insert node at the beginning ***/////
-        insertNodeAtBeginning();
-        //***  Display nodes ****//
-        displayNodes();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import LinkedList.BasicOperations;
+import LinkedList.Node;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BasicOperations basicOperations = new BasicOperations(null);
+        basicOperations.insertNodeAtEnd();
+        basicOperations.insertNodeAtBeginning();
+        basicOperations.displayNodes();
+        basicOperations.deleteNodeFromStart();
+        basicOperations.displayNodes();
     }
 }
