d6ce7fbd5a4cff7726c5051e9a7c54e17a34cbc6
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-11-24 21:42:00 +0530

    removed files

---
+++ src/Tree/BasicOperationsOnTree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class BasicOperationsOnTree {
+
+    private TreeNode headTreeNode = null;
+
+    public BasicOperationsOnTree(TreeNode headTreeNode) {
+        this.headTreeNode = headTreeNode;
+    }
+
+    public void insertNewNode(){
+
+    }
+}
